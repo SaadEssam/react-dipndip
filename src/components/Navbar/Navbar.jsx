23c347e78fe967c16c9cb3fb54501cb65f179e35
SaadEssam <SaadEssam059@gmail.com>
2023-01-24 14:32:00 +0200

    responsive-navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { TiThMenuOutline } from 'react-icons/ti';
+import { IoRestaurantOutline } from 'react-icons/io5';
 import { MdMailOutline } from 'react-icons/md';
 import images from '../../constants/images';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className="app_navbar">
       <div className="app_navbar-logo">
         <img src={images.dipndip} alt="app logo" />
       </div>
       <ul className="app_navbar-links">
-        <li className="p_opensans"><a href="#home">Home</a></li>
-        <li className="p_opensans"><a href="#about">About</a></li>
-        <li className="p_opensans"><a href="#menu">Menu</a></li>
-        <li className="p_opensans"><a href="#awards">Awards</a></li>
-        <li className="p_opensans"><a href="#contact">Contact</a></li>
+        <li className="p_ubuntu"><a href="#home">Home</a></li>
+        <li className="p_ubuntu"><a href="#about">About</a></li>
+        <li className="p_ubuntu"><a href="#menu">Menu</a></li>
+        <li className="p_ubuntu"><a href="#awards">Awards</a></li>
+        <li className="p_ubuntu"><a href="#contact">Contact</a></li>
       </ul>
       <div className="app_navbar-right">
-        <a href="#email" className="p_opensans email"><MdMailOutline fontSize={17} />Info@dipndip.com</a>
+        <a href="#email" className="p_ubuntu email"><MdMailOutline fontSize={17} />Info@dipndip.com</a>
         <div />
-        <a href="#/" className="p_opensans">Book Table</a>
+        <a href="#/" className="p_ubuntu">Book Table</a>
       </div>
       <div className="app_navbar-smallscreen">
-        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
+        <TiThMenuOutline color="#fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
+
+        {toggleMenu && (
+          <div className="app_navbar-smallscreen_overlay flex_center slide-bottom">
+            <IoRestaurantOutline fontSize={27} className="overlay_close" onClick={() => setToggleMenu(false)} />
+            <ul className="app_navbar-smallscreen_links">
+              <li className="p_ubuntu"><a href="#home">Home</a></li>
+              <li className="p_ubuntu"><a href="#about">About</a></li>
+              <li className="p_ubuntu"><a href="#menu">Menu</a></li>
+              <li className="p_ubuntu"><a href="#awards">Awards</a></li>
+              <li className="p_ubuntu"><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   )
